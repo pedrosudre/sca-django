dd9912fe6b4e090a50e1d6175552e13b7d214c4f
Pedro Sudre <pedrosudre04@gmail.com>
2022-10-27 16:25:06 -0300

    template

--- aplic/urls.py
+++ aplic/urls.py
@@ -5,3 +5,10 @@ from .views import IndexView
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
 ]
+
+from .views import IndexView, SobreView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
+]
