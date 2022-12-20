7b743bec3c3da46831e0bd8fca4d3c0e1702f28f
Pedro Sudré <pedrosudre04@gmail.com>
2022-12-20 14:44:35 -0300

    grafico

--- aplic/urls.py
+++ aplic/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import IndexView, SobreView, ProfessoresView, CursoDetalheView
+from .views import IndexView, SobreView, ProfessoresView, CursoDetalheView, DadosGraficosAlunosView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('sobre/', SobreView.as_view(), name='sobre'),
     path('professores/', ProfessoresView.as_view(), name='professores'),
     path('curso-detalhe/<int:id>/', CursoDetalheView.as_view(), name='curso-detalhe'),
+    path('dados-graficos-alunos/', DadosGraficosAlunosView.as_view(), name='dados-graficos-alunos'),
+
 ]
--- aplic/views.py
+++ aplic/views.py
@@ -2,6 +2,8 @@ from django.views.generic import TemplateView, ListView
 from .models import Professor
 from .models import Curso
 from .models import Disciplina
+from django.db.models import Count
+from chart.views.lines import BaseLineChartView
 
 
 class IndexView(TemplateView):
@@ -40,3 +42,21 @@ class CursoDetalheView(ListView):
     def get_queryset(self, **kwargs):
         id = self.kwargs['id']
         return Disciplina.objects.filter(curso_id=id)
+
+    class DadosGraficoAlunosView(BaseLineChartView):
+    
+        def get_labels(self):
+            labels = []
+            queryset = Curso.objects.order_by('id')
+            for curso in queryset:
+                labels.append(curso.nome)
+            return labels
+
+        def get_data(self):
+            resultado = []
+            dados = []
+            queryset = Curso.objects.order_by('id').annotate(total=Count('aluno'))
+            for linha in queryset:
+                dados.append(int(linha.total))
+            resultado.append(dados)
+            return resultado
